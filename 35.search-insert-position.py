6153683889c43e6c4859473366dff59cc85fa177
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-09-27 12:05:59 +0900

    35.search-insert-positionの解答

---
+++ 35.search-insert-position.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = math.floor((left+right) / 2)
+            if nums[middle] == target: return middle
+
+            if nums[middle] < target: left = middle + 1
+            else: right = middle - 1
+        
+        return left
+# @lc code=end
+
