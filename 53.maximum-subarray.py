5d3ca4df222f990ce1661675a8a40d5db68437a2
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-09-28 13:00:12 +0900

    別解

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -7,12 +7,13 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        finalMax = nums[0]
-        currentMax = nums[0]
+        currentMaxes = [0] * len(nums)
+        currentMaxes[0] = nums[0]
+        result = currentMaxes[0]
         for i in range(1, len(nums)):
-            currentMax = max(currentMax + nums[i] ,nums[i])
-            finalMax = max(currentMax ,finalMax)
+            currentMaxes[i] = nums[i] + (currentMaxes[i-1] if currentMaxes[i-1] > 0 else 0)
+            result = max(currentMaxes[i] ,result)
         
-        return finalMax
+        return result
 # @lc code=end
 
