27f64051a722e5cef5f7949921e231f84ff546e4
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-10-06 17:48:56 +0900

    要復習

---
+++ 15.3-sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if nums[i] > 0: break
+            if i > 0 and nums[i] == nums[i-1]: continue
+
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0: left += 1
+                elif total > 0: right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        
+        return results
+# @lc code=end
+
