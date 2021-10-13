4dd547775ebd5277af6fa6fa9545e2cc1948b9b3
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-10-13 12:37:03 +0900

    よりシンプルに

--- 205.isomorphic-strings.py
+++ 205.isomorphic-strings.py
@@ -7,19 +7,16 @@
 # @lc code=start
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        hashmapS = self.makeHashmap(s)
-        hashmapT = self.makeHashmap(t)
-        for (valueS, valueT) in zip(hashmapS.values(), hashmapT.values()):
-            if valueS != valueT: return False
-
+        hashmapS = {}
+        hashmapT = {}
+        for (chrS, chrT) in zip(s, t):
+            if chrS not in hashmapS and chrT not in hashmapT:
+                hashmapS[chrS] = chrT
+                hashmapT[chrT] = chrS
+            elif hashmapS.get(chrS) != chrT or hashmapT.get(chrT) != chrS:
+                return False
+                
         return True
 
-    def makeHashmap(self, str: str) -> dict[str, int]:
-        hashmap = {}
-        for i in range(len(str)):
-            if str[i] in hashmap: hashmap[str[i]] += i
-            else: hashmap[str[i]] = i
-        
-        return hashmap
 # @lc code=end
 
