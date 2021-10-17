0eb73626eeb5776f8b8a7ae8c4ac22a3cc24a974
kazuma-shinomiya <kazuma.s6145@icloud.com>
2021-10-17 17:58:18 +0900

    リストを使用

---
+++ 234.palindrome-linked-list.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=234 lang=python3
+#
+# [234] Palindrome Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+        node = head
+        while node:
+            values.append(node.val)
+            node = node.next
+        
+        return values == values[::-1]
+# @lc code=end
+
